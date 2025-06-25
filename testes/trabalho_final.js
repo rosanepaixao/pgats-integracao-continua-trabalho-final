9d367e6daf4591cfdb6a1613a9b35939b6946303
Rosane da Paixão <133041345+rosanepaixao@users.noreply.github.com>
2025-06-25 17:04:10 -0300

    ci: Alterando relatorio final

--- testes/trabalho_final.js
+++ testes/trabalho_final.js
@@ -1,16 +1,9 @@
-/*describe('Testes da disciplina - fundamentos JS', () => {
-  
-  it('QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas', () => {
-    assert.strictEqual(geradorDeTagsDeIdentificacao('Pantera'), 'PANTERA');
-  });*/
+
 
 export function geradorDeTagsDeIdentificacao(nome) {
     const pet = nome.toUpperCase()
     return pet
 }
-/*it('QUANDO a idade = 1 + porte M, DEVE ser permitida a adoção', () => {
-    assert.strictEqual(verificarSePodeSerAdotado(1, 'M'), true)
-})*/
 
 export function verificarSePodeSerAdotado(idade, porte) {
     if (idade == 1 && porte == `M`) {
@@ -19,9 +12,6 @@ export function verificarSePodeSerAdotado(idade, porte) {
     return false
 }
 
-/*it('QUANDO o peso = 14.5, DEVE ser retornado 4350 gramas para o consumo diário', () => {
-    assert.strictEqual(calcularConsumoDeRacao('Pitoco', 1, 14.5), 4350)
-  }) */
 
 export function calcularConsumoDeRacao(nome, quantidadeDias, peso) {
     const racao = peso * 300
@@ -30,9 +20,6 @@ export function calcularConsumoDeRacao(nome, quantidadeDias, peso) {
     return totalGramas
 }
 
-/*it('QUANDO o porte = pequeno, DEVE ser retornada a atividade adequada', () => {
-    assert.strictEqual(decidirTipoDeAtividadePorPorte('pequeno'), 'brincar dentro de casa')
-}) */
 
 export function decidirTipoDeAtividadePorPorte(porte) {
     switch (porte) {
@@ -49,9 +36,6 @@ export function decidirTipoDeAtividadePorPorte(porte) {
     }
 }
 
-/*it('QUANDO buscar dado de exemplo, DEVE retornar um valor de forma assíncrona', async () => {
-    const resultado = await buscarDadoAsync();
-    assert.strictEqual(resultado, 'Pipoca')*/
 
 export async function buscarDadoAsync() {
     return `Pipoca`
